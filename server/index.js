cf583394049b4302403ae0c6e5e80523316404d9
Rohith Kumar <89332475+RohithR-kumar@users.noreply.github.com>
2022-08-18 17:10:52 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const { Ads2Model, Companies2Model } = require("./models/AdsModel");
 const mongoose = require("mongoose");
 
-const url =
-  "mongodb+srv://cluster0:cluster0@cluster0.47nh2.mongodb.net/Ad?retryWrites=true&w=majority";
+const url = "<url>";
 mongoose.connect(url);
 
 const app = express();
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
 
 app.get("/", async (req, res) => {
   res.json({ message: "success" });
