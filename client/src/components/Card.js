6cf4206193af079abac08c2dea31d4971dabc459
Rohith Kumar <89332475+RohithR-kumar@users.noreply.github.com>
2022-08-18 17:14:26 +0530

    Update Card.js

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -2,10 +2,12 @@ const Card = ({ item }) => {
   return (
     <>
       <div className="card" style={{ width: "18rem" }}>
+        <h5 className="card-title">{item.headline}</h5>
         <img src={item.imageUrl} className="card-img-top" alt="Ad Card" />
         <div className="card-body">
           <h5 className="card-title">{item.name}</h5>
           <p className="card-text">{item.primaryText}</p>
+          <p className="card-text">{item.description}</p>
           <a href={item.companyId.url} className="btn btn-primary">
             {item.cta}
           </a>
